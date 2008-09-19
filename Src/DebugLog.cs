b824f23c15305c720013eb43dd5a219605d90717
Timwi <github@timwi.de>
2008-09-19 18:56:00 +0100

    add missing files (oops) (imported from Perforce changelist 522)

---
+++ Src/DebugLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Servers
+{
+    public class DebugLog
+    {
+        private string LogFile;
+        public DebugLog(string LogFileName) { LogFile = LogFileName; }
+        public void Log(string Message)
+        {
+            lock (this)
+            {
+                FileStream f = File.Open(LogFile, FileMode.Append, FileAccess.Write, FileShare.Write);
+                byte[] Msg = ("[" + DateTime.Now.ToString("r") + "] " + Message + "\r\n").ToUTF8();
+                f.Write(Msg, 0, Msg.Length);
+                f.Close();
+            }
+        }
+    }
+}
---
+++ Src/Stopwatch.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Servers
+{
+    public struct StopWatchElement
+    {
+        public double Milliseconds;
+        public string Milestone;
+    }
+
+    public abstract class Stopwatch
+    {
+        public abstract void w(string Msg);
+        public abstract void SaveToFile(string Filepath);
+    }
+
+    public class StopwatchReal : Stopwatch
+    {
+        public DateTime StartTime = DateTime.Now;
+        public List<StopWatchElement> Elements = new List<StopWatchElement>();
+        public override void w(string Msg)
+        {
+            Elements.Add(new StopWatchElement()
+            {
+                Milestone = Msg,
+                Milliseconds = (DateTime.Now - StartTime).TotalMilliseconds
+            });
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            int MaxLength = 0;
+            foreach (var x in Elements)
+                MaxLength = Math.Max(MaxLength, x.Milestone.Length + 5);
+            sb.Append(new string('-', MaxLength + 20) + "\r\n");
+            for (int i = 0; i < Elements.Count; i++)
+            {
+                var x = Elements[i];
+                sb.Append(x.Milestone.PadRight(MaxLength, '.'));
+                sb.Append(string.Format("{0,10:0.00}", (i > 0 ? x.Milliseconds - Elements[i - 1].Milliseconds : x.Milliseconds)));
+                sb.Append(string.Format("{0,10:0.00}", x.Milliseconds));
+                sb.Append("\r\n");
+            }
+            return sb.ToString();
+        }
+        public override void SaveToFile(string Filepath)
+        {
+            try
+            {
+                FileStream f = File.Open(Filepath, FileMode.Create, FileAccess.Write, FileShare.Write);
+                byte[] b = this.ToString().ToUTF8();
+                f.Write(b, 0, b.Length);
+                f.Close();
+            }
+            catch (IOException)
+            {
+            }
+        }
+    }
+
+    public class StopwatchDummy : Stopwatch
+    {
+        public override void w(string Msg) { }
+        public override string ToString() { return ""; }
+        public override void SaveToFile(string Filepath) { }
+    }
+}
