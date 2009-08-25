3ca9ccceb4a4bc80ed611d3d9fd03a9cf5ac63ab
Timwi <github@timwi.de>
2009-08-25 17:59:38 +0100

    Fix resources (imported from Perforce changelist 1186)

--- Src/Resources.Designer.cs
+++ Src/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace RT.Servers {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Servers.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RT.Servers.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
