2db6d8a34392e7ee5498c3453c965ab80ea3ac54
rstarkov <sourcecode@roman.st>
2012-04-26 16:09:42 +0100

    Restructure the logic of Session somewhat: automatically delete "junk" session cookie; suppress session delete if one wasn't read; enable updates of the cookie Expires date; support the concept of "session changed". (imported from Perforce changelist 3672)

--- Src/Session.cs
+++ Src/Session.cs
@@ -1,32 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using RT.Util;
 using RT.Util.ExtensionMethods;
 using RT.Util.Xml;
 
 namespace RT.Servers
 {
-    /// <summary>Contains values to specify what should happen to a session when it is closed.</summary>
-    public enum SessionCloseAction
-    {
-        /// <summary>The updated session is saved to the session store.</summary>
-        Save,
-        /// <summary>The updates to the session are discarded. The previous state of the session is retained.</summary>
-        Discard,
-        /// <summary>The session is deleted.</summary>
-        Delete
-    };
-
     /// <summary>Provides functionality to track user sessions in HTTP requests using cookies. See remarks for usage guidelines.</summary>
     /// <remarks>
     ///    <para>Intended use is as follows:</para>
     ///    <list type="bullet">
     ///        <item><description>Declare a class containing one or more fields which constitute your session data.</description></item>
-    ///        <item><description>Derive that class from <see cref="FileSession"/>, or implement your own alternative to <see cref="FileSession"/> and derive from that.</description></item>
+    ///        <item><description>Derive that class from <see cref="Session"/>, <see cref="FileSession"/>, or your own specialization of either.</description></item>
     ///        <item><description>
     ///            <para>You can now augment any HTTP request handler that looks like this:</para>
     ///            <code>req => { /* code to handle request, no session support */ }</code>
@@ -35,98 +21,115 @@ namespace RT.Servers
     ///            <para>(replace "TSession" with the name of your derived class; see <see cref="Enable"/>).</para>
     ///        </description></item>
     ///        <item><description>Within your request handler, you can make arbitrary changes to the session object, which will be persisted automatically.</description></item>
-    ///        <item><description>You can set session.<see cref="CloseAction"/> to a non-default value to suppress automatic session saving.</description></item>
+    ///        <item><description>Make sure that whenever a session variable is changed, <see cref="SessionModified"/> is set to true.</description></item>
+    ///        <item><description>You can set session.<see cref="Delete"/> to true to avoid creating a new session / delete the existing session.</description></item>
+    ///        <item><description>You can set session.<see cref="SessionModified"/> to false to discard any changes made to the session variables.</description></item>
     ///    </list>
     /// </remarks>
     public abstract class Session
     {
-        /// <summary>Specifies how to proceed when the session object is disposed.</summary>
+        /// <summary>True if a new session was created, or any of the session variables were modified. Derived classes should set this to true whenever a session
+        /// variable is modified. Set this to false to discard all session changes (otherwise they will be saved upon session <see cref="CleanUp"/>).</summary>
         [XmlIgnore]
-        public SessionCloseAction CloseAction = SessionCloseAction.Save;
+        public bool SessionModified { get; set; }
 
-        /// <summary>Contains the session ID (the cookie value).</summary>
+        /// <summary>True if a new session was created, or any of the cookie parameters were modified (such as <see cref="CookieExpires"/>).</summary>
         [XmlIgnore]
-        public string SessionID { get; protected set; }
+        public bool CookieModified { get; set; }
+
+        /// <summary>Set this to true to cause the session and the associated cookie to be deleted. The deletion occurs upon session <see cref="CleanUp"/>.</summary>
+        [XmlIgnore]
+        public bool Delete { get; set; }
 
         [XmlIgnore]
-        private bool _isNew;
+        private bool _hadSession, _hadCookie;
+
+        /// <summary>Contains the session ID, which is also the cookie value.</summary>
+        [XmlIgnore]
+        public string SessionID { get; private set; }
 
         /// <summary>When overridden in a derived class, gets the name of the cookie that contains the user’s session ID.</summary>
-        /// <remarks>The default implementation returns <c>this.GetType().Name</c>.</remarks>
+        /// <remarks>This property should always return the same value, otherwise the behaviour is undefined.
+        /// The default implementation returns <c>this.GetType().Name</c>.</remarks>
         protected virtual string CookieName { get { return this.GetType().Name; } }
 
         /// <summary>When overridden in a derived class, gets the applicable cookie Path for the cookie that contains the user’s session ID.</summary>
-        /// <remarks>The default implementation returns <c>"/"</c>.</remarks>
+        /// <remarks>This property should always return the same value, otherwise the behaviour is undefined.
+        /// The default implementation returns <c>"/"</c>.</remarks>
         protected virtual string CookiePath { get { return "/"; } }
 
         /// <summary>When overridden in a derived class, gets the expiry date/time of the cookie that contains the user’s session ID.</summary>
-        /// <remarks>The default implementation returns <c>DateTime.MaxValue</c>.</remarks>
+        /// <remarks>If the property value is variable, ensure that <see cref="CookieModified"/> is set to true prior to <see cref="CleanUp"/>.
+        /// The default implementation returns <c>DateTime.MaxValue</c>.</remarks>
         protected virtual DateTime CookieExpires { get { return DateTime.MaxValue; } }
 
         /// <summary>Returns a string representation of this session object.</summary>
-        public override string ToString() { return "{0} [{1}] ({2})".Fmt(SessionID, CookieName, CloseAction); }
+        public override string ToString()
+        {
+            return "{0} [{1}] ({2})".Fmt(SessionID, CookieName,
+                Delete ? "delete" : ((CookieModified ? "cookie-mod" : "") + (CookieModified && SessionModified ? ", " : "") + (SessionModified ? "session-mod" : ""))
+            );
+        }
 
-        /// <summary>When overridden in a derived class, attempts to retrieve an existing session (identified by <see cref="SessionID"/>) from the session store and initialises this instance with the relevant data.
-        /// If no such session as identified by <see cref="SessionID"/> is found in the session store, returns false.</summary>
-        protected abstract bool readSession();
+        /// <summary>When overridden in a derived class, attempts to retrieve an existing session (identified by <see cref="SessionID"/>) from the
+        /// session store and initialises this instance with the relevant data. If no such session as identified by <see cref="SessionID"/> is found in the
+        /// session store, returns false.</summary>
+        protected abstract bool ReadSession();
         /// <summary>When overridden in a derived class, saves this instance to the session store.</summary>
-        protected abstract void saveSession();
-        /// <summary>When overridden in a derived class, deletes this session (identified by <see cref="SessionID"/>) from the session store.</summary>
-        protected abstract void deleteSession();
+        protected abstract void SaveSession();
+        /// <summary>When overridden in a derived class, deletes this session (identified by <see cref="SessionID"/>) from the session store.
+        /// This method is not called if <see cref="ReadSession"/> returned false, indicating that no session was available.</summary>
+        protected abstract void DeleteSession();
 
         /// <summary>Initialises this session instance from the specified request. Only use this if you are not using <see cref="Enable{TSession}"/>, as that already calls it.</summary>
         /// <param name="req">Request containing the cookie information from which to initialise the session.</param>
-        public void InitialiseFromRequest(HttpRequest req)
+        protected void InitialiseFromRequest(HttpRequest req)
         {
-            _isNew = false;
-
             // Try to read in an existing session
-            bool done = false;
-            try
+            if (req.Headers != null && req.Headers.Cookie != null && req.Headers.Cookie.ContainsKey(CookieName))
             {
-                if (req.Headers != null && req.Headers.Cookie != null && req.Headers.Cookie.ContainsKey(CookieName))
-                {
-                    SessionID = req.Headers.Cookie[CookieName].Value;
-                    done = readSession();
-                }
+                SessionID = req.Headers.Cookie[CookieName].Value;
+                _hadSession = ReadSession();
+                _hadCookie = true;
             }
-            catch { }
 
-            // No existing session found or reading failed, so create a new session
-            if (!done)
+            // Create a new session if no existing session found
+            if (!_hadSession)
             {
                 SessionID = RndCrypto.NextBytes(21).Base64UrlEncode();
-                _isNew = true;
+                SessionModified = CookieModified = true;
             }
+            else
+                SessionModified = CookieModified = false;
         }
 
-        private void setCookie(HttpResponseHeaders headers)
+        private void saveCookie(HttpResponse response, bool delete)
         {
-            if ((_isNew && CloseAction == SessionCloseAction.Save) || (!_isNew && CloseAction == SessionCloseAction.Delete))
+            if (response.Headers.SetCookie == null)
+                response.Headers.SetCookie = new List<Cookie>();
+            response.Headers.SetCookie.Add(new Cookie
             {
-                if (headers.SetCookie == null)
-                    headers.SetCookie = new List<Cookie>();
-                headers.SetCookie.Add(new Cookie
-                {
-                    Name = CookieName,
-                    Value = CloseAction == SessionCloseAction.Delete ? "-" : SessionID,
-                    Path = CookiePath,
-                    Expires = CloseAction == SessionCloseAction.Delete ? DateTime.Today - TimeSpan.FromDays(300) : CookieExpires,
-                    HttpOnly = true,
-                });
-            }
+                Name = CookieName,
+                Value = delete ? "-" : SessionID,
+                Path = CookiePath,
+                Expires = delete ? DateTime.Today - TimeSpan.FromDays(300) : CookieExpires,
+                HttpOnly = true,
+            });
         }
 
-        private void close()
+        /// <summary>Saves/deletes the session and/or sets the session cookie, as appropriate. Only use this if you are not using <see cref="Enable{TSession}"/>, as that already calls it.</summary>
+        /// <param name="response">Response to add cookie information to.</param>
+        public virtual void CleanUp(HttpResponse response)
         {
-            try
-            {
-                if (CloseAction == SessionCloseAction.Save)
-                    saveSession();
-                else if (CloseAction == SessionCloseAction.Delete)
-                    deleteSession();
-            }
-            catch { }
+            if (Delete && _hadCookie)
+                saveCookie(response, delete: true);
+            else if (!Delete && CookieModified)
+                saveCookie(response, delete: false);
+
+            if (Delete && _hadSession)
+                DeleteSession();
+            else if (!Delete && SessionModified)
+                SaveSession();
         }
 
         /// <summary>Enables the use of sessions in an HTTP request handler.</summary>
@@ -142,14 +145,6 @@ namespace RT.Servers
             session.CleanUp(response);
             return response;
         }
-
-        /// <summary>Saves/deletes the session and/or sets the session cookie, as appropriate. Only use this if you are not using <see cref="Enable{TSession}"/>, as that already calls it.</summary>
-        /// <param name="response">Response to add cookie information to.</param>
-        public void CleanUp(HttpResponse response)
-        {
-            setCookie(response.Headers);
-            close();
-        }
     }
 
     /// <summary>Provides functionality to track user sessions in HTTP requests using cookies and to store such user sessions in the local file system.</summary>
@@ -164,7 +159,7 @@ namespace RT.Servers
         protected virtual string SessionPath { get { return Path.Combine(Path.GetTempPath(), "sessions"); } }
 
         /// <summary>Retrieves an existing session from the file system and initialises this instance with the relevant data.</summary>
-        protected override sealed bool readSession()
+        protected override sealed bool ReadSession()
         {
             var file = Path.Combine(SessionPath, SessionID);
             lock (_lock)
@@ -177,7 +172,7 @@ namespace RT.Servers
         }
 
         /// <summary>Saves this instance to the file system.</summary>
-        protected override sealed void saveSession()
+        protected override sealed void SaveSession()
         {
             var sessionPath = SessionPath;
             // Directory.CreateDirectory() does nothing if the directory already exists.
@@ -187,7 +182,7 @@ namespace RT.Servers
         }
 
         /// <summary>Deletes the file containing the data for this session from the file system.</summary>
-        protected override sealed void deleteSession()
+        protected override sealed void DeleteSession()
         {
             var path = Path.Combine(Path.GetTempPath(), "sessions");
             var sessionPath = Path.Combine(path, SessionID);
