db8fb227e6a6db9055e7f59c8735f66f1e3c1ff2
Timwi <github@timwi.de>
2013-03-09 04:28:39 +0000

    AjaxHandler: Use Classify to convert JSON values to method arguments (imported from Perforce changelist 4269)

--- Src/AjaxHandler.cs
+++ Src/AjaxHandler.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using RT.Util;
 using RT.Util.ExtensionMethods;
 using RT.Util.Json;
+using RT.Util.Serialization;
 
 namespace RT.Servers
 {
@@ -40,8 +41,7 @@ namespace RT.Servers
                     }
                     else
                     {
-                        var converter = getConverter(parameters[i].ParameterType, method.Name, paramName);
-                        parameterSetters.Add((dict, session, arr) => { arr[i] = converter(dict[paramName]); });
+                        parameterSetters.Add((dict, session, arr) => { arr[i] = Classify.ObjectFromElement(parameters[i].ParameterType, dict[paramName], ClassifyFormats.Json); });
                     }
                 }
 
@@ -54,59 +54,6 @@ namespace RT.Servers
                     return (JsonValue) method.Invoke(null, arr);
                 }));
             }
-        }
-
-        public Func<JsonValue, object> getConverter(Type targetType, string methodName, string parameterName)
-        {
-            Type[] types;
-
-            if (targetType == typeof(int))
-                return val => val.GetInt();
-            else if (targetType == typeof(long))
-                return val => val.GetLong();
-            else if (targetType == typeof(double))
-                return val => val.GetDouble();
-            else if (targetType == typeof(decimal))
-                return val => val.GetDecimal();
-            else if (targetType == typeof(bool))
-                return val => val.GetBool();
-            else if (targetType == typeof(string))
-                return val => val.GetString();
-            else if (typeof(JsonValue).IsAssignableFrom(targetType))
-                return val => val;
-            else if (targetType.TryGetInterfaceGenericParameters(typeof(ICollection<>), out types))
-            {
-                var innerType = types[0];
-                var innerConverter = getConverter(innerType, methodName, parameterName);
-                var constructor = targetType.GetConstructor(Type.EmptyTypes);
-                if (constructor == null)
-                    throw new InvalidOperationException("The AJAX method {0} has a parameter {1} which uses the type {2}, which does not have a default constructor.".Fmt(methodName, parameterName, targetType.FullName));
-                var addMethod = typeof(ICollection<>).MakeGenericType(types).GetMethod("Add");
-                return val =>
-                {
-                    var list = constructor.Invoke(null);
-                    foreach (var item in val.GetList())
-                        addMethod.Invoke(list, new object[] { innerConverter(item) });
-                    return list;
-                };
-            }
-            else if (targetType.IsArray || (targetType.IsGenericType && targetType.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
-            {
-                var innerType = targetType.IsArray ? targetType.GetElementType() : targetType.GetGenericArguments()[0];
-                var innerConverter = getConverter(innerType, methodName, parameterName);
-                var castMethod = typeof(Enumerable).GetMethod("Cast").MakeGenericMethod(innerType);
-                var toArrayMethod = typeof(Enumerable).GetMethod("ToArray").MakeGenericMethod(innerType);
-                return val => toArrayMethod.Invoke(null, new object[] { castMethod.Invoke(null, new object[] { val.GetList().Select(innerConverter) }) });
-            }
-            else if (targetType.TryGetInterfaceGenericParameters(typeof(List<>), out types))
-            {
-                var innerType = types[0];
-                var innerConverter = getConverter(innerType, methodName, parameterName);
-                var castMethod = typeof(Enumerable).GetMethod("Cast").MakeGenericMethod(innerType);
-                return val => castMethod.Invoke(null, new object[] { val.GetList().Select(innerConverter) });
-            }
-            else
-                throw new InvalidOperationException("The AJAX method {0} has a parameter {1} which uses the type {2}, which is not supported.".Fmt(methodName, parameterName, targetType.FullName));
         }
 
         public HttpResponse Handle(HttpRequest req)
