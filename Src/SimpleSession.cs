14d41c7689058ed6fb46e865809b23cc3515c383
Timwi <github@timwi.de>
2013-04-04 11:35:45 +0100

    * SimpleSession: Pass the inner handler to the Handle method instead of the constructor, and make it parameterless * SimpleSession: Support handlers that return a null response * SimpleSession: reformat comments (imported from Perforce changelist 4394)

--- Src/SimpleSession.cs
+++ Src/SimpleSession.cs
@@ -12,98 +12,93 @@ namespace RT.Servers
     /// <summary>
     ///     Provides functionality to track a session ID in HTTP requests using cookies.</summary>
     /// <remarks>
-    ///     This class only retrieves session IDs from the session cookie and does not handle mapping IDs to session data. 
-    ///     It also does not automatically create a new session if the HTTP response did not contain a session cookie.</remarks>
+    ///     This class only retrieves session IDs from the session cookie and does not handle mapping IDs to session data. It also
+    ///     does not automatically create a new session if the HTTP response did not contain a session cookie.</remarks>
     public class SimpleSession
     {
-
-
-        private Func<SimpleSession, HttpResponse> _handler;
-
         private bool _deleteSession = false;
         private bool _newSession = false;
         private string _newSessionID;
         private string _newCookiePath;
         private DateTime? _newCookieExpires;
 
-        /// <summary>
-        /// The session ID retrieved from the session cookie.
-        /// </summary>
+        /// <summary>The session ID retrieved from the session cookie.</summary>
         public string SessionID { get; private set; }
 
-        /// <summary>
-        /// The name of the cookie that contains the session ID.
-        /// </summary>
+        /// <summary>The name of the cookie that contains the session ID.</summary>
         public string CookieName { get; private set; }
 
-        /// <summary>
-        /// The path of the retrieved session cookie.
-        /// </summary>
+        /// <summary>The path of the retrieved session cookie.</summary>
         public string CookiePath { get; private set; }
 
-        /// <summary>
-        /// The expiry date of the retrieved session cookie. If null, the session expires when the client's browser closes.
-        /// </summary>
+        /// <summary>The expiry date of the retrieved session cookie. If null, the session expires when the client's browser closes.</summary>
         public DateTime? CookieExpires { get; private set; }
 
         /// <summary>
-        /// Constructs a new instance.
-        /// </summary>
-        /// <param name="handler">A function that handles the HTTP response and is called by the Handle method. 
-        /// The first parameter of the function is instantiated to the session ID, if a session exists, or null otherwise.</param>
-        public SimpleSession(Func<SimpleSession, HttpResponse> handler, string cookieName = "SessionID")
+        ///     Constructs a new instance.</summary>
+        /// <param name="cookieName">
+        ///     The name of the cookie.</param>
+        public SimpleSession(string cookieName = "SessionID")
         {
-            this._handler = handler;
-            this.CookieName = cookieName;
+            CookieName = cookieName;
         }
 
         /// <summary>
-        /// Specifies that the session cookie should be deleted by the Handle method. Its value is false initially and right after calling Handle().
-        /// </summary>
+        ///     Specifies that the session cookie should be deleted by the Handle method. Its value is false initially and right
+        ///     after calling Handle().</summary>
         public void DeleteSession()
         {
             if (_newSession)
-            {
                 throw new InvalidOperationException("Cannot both delete and create a session.");
-            }
+
             _deleteSession = true;
         }
 
         /// <summary>
-        /// Specifies that a new session should be created by the Handle method. 
-        /// </summary>
-        /// <param name="cookieExpiry">If null, the session expires when the client's browser is closed.</param>
+        ///     Specifies that a new session should be created. (The <see cref="Handle"/> method adds the relevant cookie to the
+        ///     HTTP response.)</summary>
+        /// <param name="sessionID">
+        ///     The ID for the new session.</param>
+        /// <param name="cookieExpiry">
+        ///     If null, the session expires when the client's browser is closed.</param>
+        /// <param name="cookiePath">
+        ///     The HTTP URL path to which the cookie should apply.</param>
         public void NewSession(string sessionID, DateTime? cookieExpiry, string cookiePath = "/")
         {
             if (_deleteSession)
-            {
                 throw new InvalidOperationException("Cannot both delete and create a session.");
-            }
+
             _newSession = true;
             _newSessionID = sessionID;
             _newCookiePath = cookiePath;
             _newCookieExpires = cookieExpiry;
         }
 
-
-        /// <param name="sessionID">Returns a random base 64 and URL encoded string of 21 bytes.</param>
+        /// <summary>Returns a random string of 32 characters useful as a session ID.</summary>
         public static string CreateNewSessionId()
         {
-            return RndCrypto.NextBytes(21).Base64UrlEncode();
+            return RndCrypto.GenerateString(32);
         }
 
-
-
         /// <summary>
-        /// This method retrieves the session ID from the session cookie (specified by the cookie name) from the given <see cref="HttpRequest"/> 
-        /// and calls the handler function (specified in the constructor) on the ID and the request. 
-        /// A session cookie is added to the HTTP response returned by the handler only if either DeleteSession() or NewSession() have been called (possibly by the handler). 
-        /// Before the possibly augmented HTTP response is returned, the status of DeleteSession and NewSession is reset.
-        /// </summary>
-        /// <param name="req"></param>
-        /// <returns>An HTTP response, possibly augmented with a session cookie.</returns>
-        public HttpResponse Handle(HttpRequest req)
+        ///     Retrieves the session ID from the session cookie (specified by the cookie name) from the given <see
+        ///     cref="HttpRequest"/> and calls the <paramref name="handler"/> function. A session cookie is added to the HTTP
+        ///     response returned by the handler only if either <see cref="DeleteSession"/> or <see cref="NewSession"/> have been
+        ///     called (possibly by the handler). Before the possibly augmented HTTP response is returned, the status of
+        ///     DeleteSession and NewSession is reset.</summary>
+        /// <param name="req">
+        ///     The current HTTP request.</param>
+        /// <param name="handler">
+        ///     The inner request handler to execute.</param>
+        /// <returns>
+        ///     An HTTP response, possibly augmented with a session cookie.</returns>
+        public HttpResponse Handle(HttpRequest req, Func<HttpResponse> handler)
         {
+            if (req == null)
+                throw new ArgumentNullException("req");
+            if (handler == null)
+                throw new ArgumentNullException("handler");
+
             if (req.Headers != null && req.Headers.Cookie != null && req.Headers.Cookie.ContainsKey(CookieName))
             {
                 var cookie = req.Headers.Cookie[CookieName];
@@ -116,48 +111,26 @@ namespace RT.Servers
                 SessionID = null;
             }
 
-            var response = _handler(this);
+            var response = handler();
 
-            if ((_deleteSession && SessionID != null) || _newSession)
+            if (response != null && ((_deleteSession && SessionID != null) || _newSession))
             {
                 if (response.Headers.SetCookie == null)
-                {
                     response.Headers.SetCookie = new List<Cookie>();
-                }
 
-                Cookie cookie;
-                if (_deleteSession)
+                response.Headers.SetCookie.Add(new Cookie
                 {
-                    cookie = new Cookie
-                    {
-                        Name = CookieName,
-                        Value = "-",
-                        Path = CookiePath,
-                        Expires = new DateTime(1970, 1, 1),
-                        HttpOnly = true,
-                    };
-                }
-                else // _newSession is true
-                {
-                    cookie = new Cookie
-                    {
-                        Name = CookieName,
-                        Value = _newSessionID,
-                        Path = _newCookiePath,
-                        Expires =_newCookieExpires,
-                        HttpOnly = true,
-                    };
-                }
-
-                response.Headers.SetCookie.Add(cookie);
-
+                    Name = CookieName,
+                    Value = _deleteSession ? "-" : _newSessionID,
+                    Path = _deleteSession ? CookiePath : _newCookiePath,
+                    Expires = _deleteSession ? new DateTime(1970, 1, 1) : _newCookieExpires,
+                    HttpOnly = true,
+                });
             }
+
             _deleteSession = false;
             _newSession = false;
             return response;
         }
-
-
     }
-
 }
