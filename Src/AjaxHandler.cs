6d4eb23871b98511f1dae774f5f841e133a902ef
Timwi <github@timwi.de>
2013-06-13 13:54:53 +0100

    AjaxHandler: * Remove the "wrapper" functionality; it is no longer needed now that we have AjaxHandlerOptions * Declare a hierarchy of exceptions and use them all when errors occur during AJAX handling (imported from Perforce changelist 4601)

--- Src/AjaxHandler.cs
+++ Src/AjaxHandler.cs
@@ -18,40 +18,42 @@ namespace RT.Servers
     {
         private readonly Dictionary<string, Func<HttpRequest, TApi, JsonValue>> _apiFunctions;
         private readonly AjaxHandlerOptions _options;
-        private readonly Func<Func<JsonValue>, JsonValue> _wrapper;
 
         /// <summary>
         ///     Constructs a new instance of <see cref="AjaxHandler{TApi}"/>.</summary>
         /// <param name="options">
         ///     Specifies <see cref="AjaxHandler{TApi}"/>’s exception behaviour.</param>
-        /// <param name="wrapper">
-        ///     If not <c>null</c>, provides a function in which to wrap every API function call.</param>
-        public AjaxHandler(AjaxHandlerOptions options = AjaxHandlerOptions.ReturnExceptionsWithoutMessages, Func<Func<JsonValue>, JsonValue> wrapper = null)
+        public AjaxHandler(AjaxHandlerOptions options = AjaxHandlerOptions.ReturnExceptionsWithoutMessages)
         {
             _apiFunctions = new Dictionary<string, Func<HttpRequest, TApi, JsonValue>>();
             _options = options;
-            _wrapper = wrapper;
 
             var typeContainingAjaxMethods = typeof(TApi);
             foreach (var method in typeContainingAjaxMethods.GetMethods(BindingFlags.Public | BindingFlags.Instance).Where(m => m.IsDefined<AjaxMethodAttribute>()))
             {
-                var parameterSetters = new List<Action<JsonDict, object[]>>();
                 var parameters = method.GetParameters();
-
-                for (int iFor = 0; iFor < parameters.Length; iFor++)
-                {
-                    var i = iFor;
-                    var paramName = parameters[i].Name;
-                    parameterSetters.Add((dict, arr) => { arr[i] = ClassifyJson.Deserialize(parameters[i].ParameterType, dict[paramName]); });
-                }
+                var returnType = method.ReturnType;
 
                 _apiFunctions.Add(method.Name, (req, api) =>
                 {
-                    var json = JsonDict.Parse(req.Post["data"].Value);
+                    JsonDict json;
+                    var rawJson = req.Post["data"].Value;
+                    try { json = JsonDict.Parse(rawJson); }
+                    catch (Exception e) { throw new AjaxInvalidParameterDataException(rawJson, e); }
                     var arr = new object[parameters.Length];
-                    foreach (var setter in parameterSetters)
-                        setter(json, arr);
-                    return ClassifyJson.Serialize(method.ReturnType, method.Invoke(api, arr));
+                    for (int i = 0; i < parameters.Length; i++)
+                    {
+                        var paramName = parameters[i].Name;
+                        try { arr[i] = ClassifyJson.Deserialize(parameters[i].ParameterType, json[paramName]); }
+                        catch (Exception e) { throw new AjaxInvalidParameterException(paramName, e); }
+                    }
+
+                    object result;
+                    try { result = method.Invoke(api, arr); }
+                    catch (Exception e) { throw new AjaxException("Error invoking the AJAX method.", e); }
+
+                    try { return ClassifyJson.Serialize(returnType, result); }
+                    catch (Exception e) { throw new AjaxInvalidReturnValueException(result, returnType, e); }
                 });
             }
         }
@@ -65,36 +67,35 @@ namespace RT.Servers
         public HttpResponse Handle(HttpRequest req, TApi api)
         {
             if (_options == AjaxHandlerOptions.PropagateExceptions)
-            {
-                var apiFunction = _apiFunctions[req.Post["apiFunction"].Value];
-                return HttpResponse.Json(new JsonDict
-                {
-                    { "result", _wrapper == null ? apiFunction(req, api) : _wrapper(() => apiFunction(req, api)) },
-                    { "status", "ok" }
-                });
-            }
-            else
-            {
-                try
-                {
-                    var apiFunction = _apiFunctions[req.Post["apiFunction"].Value];
-                    return HttpResponse.Json(new JsonDict
-                    {
-                        { "result", _wrapper == null ? apiFunction(req, api) : _wrapper(() => apiFunction(req, api)) },
-                        { "status", "ok" }
-                    });
-                }
-                catch (Exception e)
-                {
-                    while (e is TargetInvocationException)
-                        e = e.InnerException;
+                return callApiFunction(req, api);
 
-                    var error = new JsonDict { { "status", "error" } };
-                    if (_options == AjaxHandlerOptions.ReturnExceptionsWithMessages)
-                        error.Add("message", "{0} ({1})".Fmt(e.Message, e.GetType().Name));
-                    return HttpResponse.Json(error);
-                }
+            try
+            {
+                return callApiFunction(req, api);
             }
+            catch (Exception e)
+            {
+                while (e is TargetInvocationException)
+                    e = e.InnerException;
+
+                var error = new JsonDict { { "status", "error" } };
+                if (_options == AjaxHandlerOptions.ReturnExceptionsWithMessages)
+                    error.Add("message", "{0} ({1})".Fmt(e.Message, e.GetType().Name));
+                return HttpResponse.Json(error);
+            }
+        }
+
+        private HttpResponse callApiFunction(HttpRequest req, TApi api)
+        {
+            var apiFunctionName = req.Post["apiFunction"].Value;
+            if (apiFunctionName == null || !_apiFunctions.ContainsKey(apiFunctionName))
+                throw new AjaxMethodNotFoundException(apiFunctionName);
+            var result = _apiFunctions[apiFunctionName](req, api);
+            return HttpResponse.Json(new JsonDict
+            {
+                { "result", result },
+                { "status", "ok" }
+            });
         }
     }
 
@@ -114,5 +115,69 @@ namespace RT.Servers
         ///     <see cref="AjaxHandler{TApi}"/> does not catch exceptions thrown by the API functions. If you do not catch the
         ///     exceptions either, they will bring down the server.</summary>
         PropagateExceptions
+    }
+
+    public class AjaxException : Exception
+    {
+        public AjaxException() { }
+        public AjaxException(string message) : base(message) { }
+        public AjaxException(string message, Exception inner) : base(message, inner) { }
+    }
+
+    public class AjaxMethodNotFoundException : Exception
+    {
+        public string AjaxMethodName { get; private set; }
+
+        public AjaxMethodNotFoundException(string ajaxMethodName) : this(ajaxMethodName, "The specified AJAX method {0} does not exist.".Fmt(ajaxMethodName), null) { }
+        public AjaxMethodNotFoundException(string ajaxMethodName, string message) : this(ajaxMethodName, message, null) { }
+        public AjaxMethodNotFoundException(string ajaxMethodName, string message, Exception inner)
+            : base(message, inner)
+        {
+            AjaxMethodName = ajaxMethodName;
+        }
+    }
+
+    public class AjaxInvalidParameterException : Exception
+    {
+        public string ParameterName { get; private set; }
+
+        public AjaxInvalidParameterException(string parameterName) : this(parameterName, (Exception) null) { }
+        public AjaxInvalidParameterException(string parameterName, string message) : this(parameterName, message, null) { }
+        public AjaxInvalidParameterException(string parameterName, Exception inner) : this(parameterName, "The parameter {0} could not be deseralized.".Fmt(parameterName), inner) { }
+        public AjaxInvalidParameterException(string parameterName, string message, Exception inner)
+            : base(message, inner)
+        {
+            ParameterName = parameterName;
+        }
+    }
+
+    public class AjaxInvalidParameterDataException : Exception
+    {
+        public string ParameterData { get; private set; }
+
+        public AjaxInvalidParameterDataException(string parameterData) : this(parameterData, (Exception) null) { }
+        public AjaxInvalidParameterDataException(string parameterData, string message) : this(parameterData, message, null) { }
+        public AjaxInvalidParameterDataException(string parameterData, Exception inner) : this(parameterData, "The provided JSON for the method paramters is not a valid JSON dictionary.", inner) { }
+        public AjaxInvalidParameterDataException(string parameterData, string message, Exception inner)
+            : base(message, inner)
+        {
+            ParameterData = parameterData;
+        }
+    }
+
+    public class AjaxInvalidReturnValueException : Exception
+    {
+        public object ReturnValue { get; private set; }
+        public Type ReturnType { get; private set; }
+
+        public AjaxInvalidReturnValueException(object returnValue, Type returnType) : this(returnValue, returnType, (Exception) null) { }
+        public AjaxInvalidReturnValueException(object returnValue, Type returnType, string message) : this(returnValue, returnType, message, null) { }
+        public AjaxInvalidReturnValueException(object returnValue, Type returnType, Exception inner) : this(returnValue, returnType, "The return value of the AJAX call could not be serialized.", inner) { }
+        public AjaxInvalidReturnValueException(object returnValue, Type returnType, string message, Exception inner)
+            : base(message, inner)
+        {
+            ReturnValue = returnValue;
+            ReturnType = returnType;
+        }
     }
 }
