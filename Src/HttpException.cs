6edc9d369a53f4cd404bccfe510cd1751d4b7ed4
Timwi <github@timwi.de>
2013-09-12 10:06:22 +0100

    Only documentation fixes and compiler warning fixes. (imported from Perforce changelist 5054)

--- Src/HttpException.cs
+++ Src/HttpException.cs
@@ -13,10 +13,15 @@ namespace RT.Servers
         /// <summary>An error message which may be revealed to the user.</summary>
         public string UserMessage { get; private set; }
 
-        /// <summary>Constructor.</summary>
-        /// <param name="statusCode">The status code associated with this exception.</param>
-        /// <param name="message">An optional exception message. If omitted, the <paramref name="userMessage"/> will be used.</param>
-        /// <param name="userMessage">An optional error message which may be revealed to the user. If omitted, a default status code description will be used.</param>
+        /// <summary>
+        ///     Constructor.</summary>
+        /// <param name="statusCode">
+        ///     The status code associated with this exception.</param>
+        /// <param name="message">
+        ///     An optional exception message. If omitted, the <paramref name="userMessage"/> will be used.</param>
+        /// <param name="userMessage">
+        ///     An optional error message which may be revealed to the user. If omitted, a default status code description
+        ///     will be used.</param>
         public HttpException(HttpStatusCode statusCode, string message = null, string userMessage = null)
             : base(message ?? userMessage ?? statusCode.ToText())
         {
@@ -24,6 +29,7 @@ namespace RT.Servers
             UserMessage = userMessage ?? statusCode.ToText();
         }
 
+        /// <summary>Constructor for serialization.</summary>
         protected HttpException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
@@ -31,6 +37,7 @@ namespace RT.Servers
             UserMessage = info.GetString("UserMessage");
         }
 
+        /// <summary>Override for serialization.</summary>
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             base.GetObjectData(info, context);
@@ -46,20 +53,24 @@ namespace RT.Servers
         /// <summary>A string describing the resource that was not found. May be null.</summary>
         public string Location { get; private set; }
 
-        /// <summary>Constructor.</summary>
-        /// <param name="location">An optional string describing the resource that was not found. This will be revealed to the user.</param>
+        /// <summary>
+        ///     Constructor.</summary>
+        /// <param name="location">
+        ///     An optional string describing the resource that was not found. This will be revealed to the user.</param>
         public HttpNotFoundException(string location = null)
             : base(HttpStatusCode._404_NotFound, null, location == null ? null : ("“" + location + "” does not exist."))
         {
             Location = location;
         }
 
+        /// <summary>Constructor for serialization.</summary>
         protected HttpNotFoundException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
             Location = info.GetString("Location");
         }
 
+        /// <summary>Override for serialization.</summary>
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             base.GetObjectData(info, context);
@@ -67,7 +78,9 @@ namespace RT.Servers
         }
     }
 
-    /// <summary>Indicates that an error has occurred while parsing a request. This usually indicates that the request was malformed in some way.</summary>
+    /// <summary>
+    ///     Indicates that an error has occurred while parsing a request. This usually indicates that the request was
+    ///     malformed in some way.</summary>
     [Serializable]
     public class HttpRequestParseException : HttpException
     {
@@ -75,6 +88,7 @@ namespace RT.Servers
         internal HttpRequestParseException(HttpStatusCode statusCode, string userMessage = null)
             : base(statusCode, null, userMessage) { }
 
+        /// <summary>Constructor for serialization.</summary>
         protected HttpRequestParseException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
