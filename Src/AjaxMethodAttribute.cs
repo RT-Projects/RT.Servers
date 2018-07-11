e0ced8e9941003d3b39193a9fce27173c498ca45
Timwi <github@timwi.de>
2018-07-11 20:46:55 +0200

    Minor XML comment fix.

--- Src/AjaxMethodAttribute.cs
+++ Src/AjaxMethodAttribute.cs
@@ -2,7 +2,7 @@
 
 namespace RT.Servers
 {
-    /// <summary>Specifies that a method can be called via AJAX using an <see cref="AjaxHandler{TSession}"/>.</summary>
+    /// <summary>Specifies that a method can be called via AJAX using an <see cref="AjaxHandler{TApi}"/>.</summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     public class AjaxMethodAttribute : Attribute
     {
